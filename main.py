7fd862adaf54d8a72f27e58564d90e14f2b6054d
Brickuser24 <151051795+Brickuser24@users.noreply.github.com>
2024-06-19 12:13:19 +0530

    Update main.py
    Add command logs

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ client = commands.Bot(command_prefix="!", intents=nextcord.Intents.all())
 async def on_ready():
     await client.change_presence(activity=nextcord.Game("Bedwars"))
     print("\nR.A.L.P.H\nStatus: Online\nAwaiting Prompt\n")
+
+@client.event
+async def on_command(ctx):
+    command_name = ctx.command.name
+    user = ctx.author
+    channel = ctx.channel
+    with open("logs.txt", "a") as f:
+      f.write(f"Command '{command_name}' was used by {user} in {channel}."+"\n")
+      f.close()
   
 initial_extensions=[]
 for filename in os.listdir('./cogs'):
