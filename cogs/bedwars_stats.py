13a2ab251f15a3346eb1b14fe3075627d4243609
Brickuser24 <151051795+Brickuser24@users.noreply.github.com>
2024-09-27 22:27:31 +0530

    Update bedwars_stats.py

--- cogs/bedwars_stats.py
+++ cogs/bedwars_stats.py
@@ -18,19 +18,7 @@ class bedwars_stats(commands.Cog):
         querystring = {"type":"bedwars","interval":"total","mode":"ALL_MODES"}
         headers = {
             "authority": "stats.pika-network.net",
-            "accept": "*/*",
-            "accept-language": "en-US,en;q=0.9",
-            "cache-control": "no-cache",
-            "cookie": "_ga=GA1.1.434310172.1706248472; __gads=ID=976420f80d2fde84:T=1706248472:RT=1706248472:S=ALNI_MZv-YEMQrzO8S21pJZCTJ8eM9aUxw; __gpi=UID=00000cf05c5deecc:T=1706248472:RT=1706248472:S=ALNI_Mb0dwnF9AEPJ0PPpgxTwBhxDPwIIw; _ga_LRYLYJ15WQ=GS1.1.1706248471.1.0.1706248481.0.0.0; _ga_ZXJL7HEGYW=GS1.1.1706444920.2.1.1706444985.0.0.0",
-            "pragma": "no-cache",
-            "referer": f"https://stats.pika-network.net/player/{user}/bedwars",
-            "sec-ch-ua": "^\^Not_A",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "^\^Windows^^",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+            "referer": f"https://stats.pika-network.net/player/{user}/bedwars"
         }
         response = requests.request("GET", url, headers=headers, params=querystring)
         stats=response.json()
